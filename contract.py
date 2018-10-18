58ab92c67801552f2b49736a345b25e9ae07e809
Karl Floersch <karl@karlfloersch.com>
2018-10-18 05:11:28 -0400

    Add merkle proof verification

--- contract.py
+++ contract.py
@@ -1,3 +1,7 @@
+from ethereum.utils import sha3, int_to_bytes, bytes_to_int
+from tx_tree_utils import get_sum_hash_of_tx
+
+
 EXIT_CHALLENGE_PERIOD = 10
 EXIT_REFUTATION_GRACE_PERIOD = 15
 
@@ -8,6 +12,7 @@ class PlasmaContract():
         self.blocks = []
         self.total_deposits = {}
         self.deposits = {}
+        self.exited_coin_ranges = []
         self.time = 0
         self.exits = []
 
@@ -40,30 +45,51 @@ class PlasmaContract():
             'start': start,
             'offset': offset,
             'challenge_blocks': [],
+            'challenge_txs': [],
             'owner': owner
         })
         # TODO: Add bond for exit
 
-    def challenge_spent_coin(self, exit_index, coin_id, blocknumber_of_spend, proof):
+    def verify_merkle_proof(self, blockhash, proof):
+        h = b''
+        print(proof)
+        for i in range(0, len(proof) - 1, 2):
+            new_value = b''.join([proof[i], proof[i+1]])
+            new_sum = int_to_bytes(bytes_to_int(proof[i][32:]) + bytes_to_int(proof[i+1][32:])).rjust(8, b"\x00")
+            h = b''.join([sha3(new_value), new_sum])
+            assert h == proof[i+2]
+        return h == proof[-1]
+
+    def challenge_coin(self, exit_index, coin_id, blocknumber_of_spend, tx, proof):
         # Check that the challenge period isn't up
         assert self.time < self.exits[exit_index]['timestamp'] + EXIT_CHALLENGE_PERIOD
         # Check that the challenge is of the correct coin
-        assert self.exits[exit_index]['start'] < coin_id and coin_id < self.exits[exit_index]['start'] + self.exits[exit_index]['offset']
-        # TODO: Actually implement proof checker
-        if proof is False:
-            return
+        assert self.exits[exit_index]['start'] <= coin_id and coin_id < self.exits[exit_index]['start'] + self.exits[exit_index]['offset']
+        # Check that the transaction hash is in the proof
+        assert get_sum_hash_of_tx(tx) in proof
+        # Check that the transaction is in the specified block
+        assert self.verify_merkle_proof(self.blocks[blocknumber_of_spend], proof)
+        # Record challenge
         self.exits[exit_index]['challenge_blocks'].append(blocknumber_of_spend)
+        self.exits[exit_index]['challenge_txs'].append(get_sum_hash_of_tx(tx))
 
-    def refute_challenge(self, exit_index, challenge_index, coin_id, blocknumber_of_spend, proof):
+    def refute_challenge(self, exit_index, challenge_index, coin_id, blocknumber_of_spend, tx, proof):
         # Check that the challenge is of the correct coin
-        assert self.exits[exit_index]['start'] < coin_id and coin_id < self.exits[exit_index]['start'] + self.exits[exit_index]['offset']
+        assert self.exits[exit_index]['start'] <= coin_id and coin_id < self.exits[exit_index]['start'] + self.exits[exit_index]['offset']
+        # Check that the blocknumber of the refutation is after the challenge block
         assert blocknumber_of_spend > self.exits[exit_index]['challenge_blocks'][challenge_index]
-        # TODO: Implement proof checker--Check inclusion & that the tx spends the challenge coin
-        if proof is False:
-            return
+        # Check that the transaction hash is in the proof
+        assert get_sum_hash_of_tx(tx) in proof
+        # Check that the transaction is in the specified block
+        assert self.verify_merkle_proof(self.blocks[blocknumber_of_spend], proof)
+        # Check that the transaction references the challenge tx
+        assert tx['contents']['prev_tx'] == str(self.exits[exit_index]['challenge_txs'][challenge_index])
+        # Delete invalid challenge. TODO: Forfit bond
         del self.exits[exit_index]['challenge_blocks'][challenge_index]
+        del self.exits[exit_index]['challenge_txs'][challenge_index]
 
     def withdraw(self, exit_index):
         assert self.time > self.exits[exit_index]['timestamp'] + EXIT_REFUTATION_GRACE_PERIOD
         assert len(self.exits[exit_index]['challenge_blocks']) == 0
         print('successful withdrawl! Sending money to ', self.exits[exit_index]['owner'])
+        self.exited_coin_ranges.append([self.exits[exit_index]['start'], self.exits[exit_index]['offset']])
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from contract import PlasmaContract
-from tx_tree_utils import EphemDB, fill_tx_list_with_notxs, get_sum_hash_of_tx, make_block_from_txs
+from tx_tree_utils import EphemDB, fill_tx_list_with_notxs, make_block_from_txs, get_proof_of_index, get_sum_hash_of_tx
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -13,8 +13,9 @@ block0_full_txs = fill_tx_list_with_notxs(block0_txs)
 block0_hash = make_block_from_txs(db, block0_full_txs)
 pp.pprint(block0_full_txs)
 
+block1_tx_hash = get_sum_hash_of_tx(block0_txs[0])
 block1_txs = [
-    {'type:': 'send', 'contents': {'start': 2, 'offset': 1, 'owner': 'alice', 'to': 'bob'}, 'sig': 'sig'},
+    {'type:': 'send', 'contents': {'prev_tx': str(block1_tx_hash), 'start': 2, 'offset': 2, 'owner': 'alice', 'to': 'bob'}, 'sig': 'sig'},
 ]
 block1_full_txs = fill_tx_list_with_notxs(block1_txs)
 block1_hash = make_block_from_txs(db, block1_full_txs)
@@ -26,12 +27,14 @@ contract.add_block(block0_hash)
 contract.add_block(block1_hash)
 contract.time += 5
 # Start an exit referencing a spent coin
-contract.exit('ETH', 1, 2, 'alice')
+contract.exit('ETH', 2, 2, 'bob')
 contract.time += 5
-# Challenge the exit with Bob's tx -- coin 2 in block 2
-contract.challenge_spent_coin(0, 2, 0, 'proof')
-contract.refute_challenge(0, 0, 2, 1, 'proof')
+# Challenge the exit with Alice's tx -- coin 2 in block 0
+challenge_proof = get_proof_of_index(db, block0_hash, 2)
+contract.challenge_coin(0, 2, 0, block0_txs[0], challenge_proof)
+# Refute the challenge with Bob's tx -- coin 2 in block 1
+refute_proof = get_proof_of_index(db, block1_hash, 2)
+contract.refute_challenge(0, 0, 2, 1, block1_txs[0], refute_proof)
 contract.time += 15
+# Withdraw the coin successfully!
 contract.withdraw(0)
-# print(contract.total_deposits)
-# print(contract.blocks)
