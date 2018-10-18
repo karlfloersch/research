228f1b0fd891b34ccef167093f35e6003cd0db39
Karl Floersch <karl@karlfloersch.com>
2018-10-18 03:55:49 -0400

    Add basic challenge response

--- contract.py
+++ contract.py
@@ -1,3 +1,5 @@
+EXIT_CHALLENGE_PERIOD = 10
+EXIT_REFUTATION_GRACE_PERIOD = 15
 
 class PlasmaContract():
 
@@ -6,6 +8,8 @@ class PlasmaContract():
         self.blocks = []
         self.total_deposits = {}
         self.deposits = {}
+        self.time = 0
+        self.exits = []
 
     def deposit(self, token, amount, owner):
         if amount < 1:
@@ -28,3 +32,38 @@ class PlasmaContract():
     def add_block(self, blockhash):
         self.blocks.append(blockhash)
         self.blocknumber += 1
+
+    def exit(self, token, start, offset, owner):
+        assert start + offset < self.total_deposits[token]
+        self.exits.append({
+            'timestamp': self.time,
+            'start': start,
+            'offset': offset,
+            'challenge_blocks': [],
+            'owner': owner
+        })
+        # TODO: Add bond for exit
+
+    def challenge_spent_coin(self, exit_index, coin_id, blocknumber_of_spend, proof):
+        # Check that the challenge period isn't up
+        assert self.time < self.exits[exit_index]['timestamp'] + EXIT_CHALLENGE_PERIOD
+        # Check that the challenge is of the correct coin
+        assert self.exits[exit_index]['start'] < coin_id and coin_id < self.exits[exit_index]['start'] + self.exits[exit_index]['offset']
+        # TODO: Actually implement proof checker
+        if proof is False:
+            return
+        self.exits[exit_index]['challenge_blocks'].append(blocknumber_of_spend)
+
+    def refute_challenge(self, exit_index, challenge_index, coin_id, blocknumber_of_spend, proof):
+        # Check that the challenge is of the correct coin
+        assert self.exits[exit_index]['start'] < coin_id and coin_id < self.exits[exit_index]['start'] + self.exits[exit_index]['offset']
+        assert blocknumber_of_spend > self.exits[exit_index]['challenge_blocks'][challenge_index]
+        # TODO: Implement proof checker--Check inclusion & that the tx spends the challenge coin
+        if proof is False:
+            return
+        del self.exits[exit_index]['challenge_blocks'][challenge_index]
+
+    def withdraw(self, exit_index):
+        assert self.time > self.exits[exit_index]['timestamp'] + EXIT_REFUTATION_GRACE_PERIOD
+        assert len(self.exits[exit_index]['challenge_blocks']) == 0
+        print('successful withdrawl! Sending money to ', self.exits[exit_index]['owner'])
--- test.py
+++ test.py
@@ -1,7 +1,37 @@
 from contract import PlasmaContract
+from tx_tree_utils import EphemDB, fill_tx_list_with_notxs, get_sum_hash_of_tx, make_block_from_txs
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 contract = PlasmaContract()
-contract.deposit('test', 100, '0xme')
-print(contract.total_deposits)
-contract.add_block('0xblockhash-test')
-print(contract.blocks)
+db = EphemDB()
+
+block0_txs = [
+    {'type:': 'send', 'contents': {'start': 1, 'offset': 4, 'owner': 'alice', 'to': 'alice'}, 'sig': 'sig'}
+]
+block0_full_txs = fill_tx_list_with_notxs(block0_txs)
+block0_hash = make_block_from_txs(db, block0_full_txs)
+pp.pprint(block0_full_txs)
+
+block1_txs = [
+    {'type:': 'send', 'contents': {'start': 2, 'offset': 1, 'owner': 'alice', 'to': 'bob'}, 'sig': 'sig'},
+]
+block1_full_txs = fill_tx_list_with_notxs(block1_txs)
+block1_hash = make_block_from_txs(db, block1_full_txs)
+pp.pprint(block1_full_txs)
+
+# Add some blocks to our Plasma contract
+contract.deposit('ETH', 10, 'alice')
+contract.add_block(block0_hash)
+contract.add_block(block1_hash)
+contract.time += 5
+# Start an exit referencing a spent coin
+contract.exit('ETH', 1, 2, 'alice')
+contract.time += 5
+# Challenge the exit with Bob's tx -- coin 2 in block 2
+contract.challenge_spent_coin(0, 2, 0, 'proof')
+contract.refute_challenge(0, 0, 2, 1, 'proof')
+contract.time += 15
+contract.withdraw(0)
+# print(contract.total_deposits)
+# print(contract.blocks)
