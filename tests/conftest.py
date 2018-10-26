dc0c1b806620750ff67ba933ce6bdd9ea7246d77
River <river.keefer@gmail.com>
2018-10-26 00:53:03 +0000

    add exit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,31 @@
 import pytest
-
 from web3 import Web3
 from eth_tester import EthereumTester, PyEVMBackend
 from vyper import compiler
 import os
+import pprint
 
-@pytest.fixture
-def plasmaprime():
-    tester = EthereumTester(backend=PyEVMBackend())
+@pytest.fixture(scope="module")
+def tester():
+    return EthereumTester(backend=PyEVMBackend())
+
+@pytest.fixture(scope="module")
+def w3(tester):
     w3 = Web3(Web3.EthereumTesterProvider(tester))
     w3.eth.setGasPriceStrategy(lambda web3, params: 0)
+    w3.eth.defaultAccount = w3.eth.accounts[0]
+    return w3
 
+@pytest.fixture
+def accounts(w3):
+    return iter(w3.eth.accounts)
+
+@pytest.fixture(scope="module")
+def pp():
+    return pprint.PrettyPrinter(indent=4)
+
+@pytest.fixture
+def plasmaprime(w3):
     wd = os.path.dirname(os.path.realpath(__file__))
     with open(os.path.join(wd, os.pardir, 'contracts/plasmaprime.vy')) as f:
         source = f.read()
--- tests/test_contract.py
+++ tests/test_contract.py
@@ -1,13 +1,58 @@
-from pprint import PrettyPrinter
 
-# pp = PrettyPrinter(indent=4)
+EXIT_PERIOD = 20
+DEPOSIT_VALUE = 1039
+EXIT_VALUE = 157
+
+from eth_tester.exceptions import TransactionFailed
+import pytest
 
 def test_compilation(plasmaprime):
     assert plasmaprime
 
-def test_deposits(plasmaprime):
-    for i in range(10):
-        plasmaprime.functions.deposit().transact({'value': i})
-    total_deposits = plasmaprime.functions.total_deposits().call()
-    assert total_deposits == 45
+def test_deposits(plasmaprime, w3):
+    for account in w3.eth.accounts:
+        tx_hash = plasmaprime.functions.deposit().transact({'from': account, 'value': DEPOSIT_VALUE})
+        tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+        assert plasmaprime.functions.deposits(account).call() == DEPOSIT_VALUE
 
+    assert plasmaprime.functions.total_deposits().call() == DEPOSIT_VALUE * len(w3.eth.accounts)
+
+def test_exits(plasmaprime, w3, tester):
+    w3.eth.defaultAccount = w3.eth.accounts[1]
+
+    # deposit
+    plasmaprime.functions.deposit().transact({'value': DEPOSIT_VALUE})
+
+    # exit params
+    plasma_block = 0
+    start = 0
+    offset = EXIT_VALUE
+
+    # submit exit
+    exit_id = plasmaprime.functions.submit_exit(plasma_block, start, offset).call()
+    tx_hash = plasmaprime.functions.submit_exit(plasma_block, start, offset).transact()
+    tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+
+    assert plasmaprime.functions.exits__owner(exit_id).call() == w3.eth.defaultAccount
+    assert plasmaprime.functions.exits__plasma_block(exit_id).call() == plasma_block
+    assert plasmaprime.functions.exits__start(exit_id).call() == start
+    assert plasmaprime.functions.exits__offset(exit_id).call() == offset
+    assert plasmaprime.functions.exit_nonce().call() == exit_id + 1
+
+    # try to finalize exit before challenge period is over
+    start_balance = w3.eth.getBalance(w3.eth.defaultAccount)
+    with pytest.raises(TransactionFailed):
+        tx_hash = plasmaprime.functions.finalize_exit(exit_id).transact()
+        tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    end_balance = w3.eth.getBalance(w3.eth.defaultAccount)
+    assert start_balance == end_balance
+
+    # mine blocks until the challenge period is over
+    blocks = tester.mine_blocks(num_blocks=EXIT_PERIOD)
+
+    # try again to finalize exit
+    start_balance = w3.eth.getBalance(w3.eth.defaultAccount)
+    tx_hash = plasmaprime.functions.finalize_exit(exit_id).transact()
+    tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    end_balance = w3.eth.getBalance(w3.eth.defaultAccount)
+    assert end_balance - start_balance == offset
