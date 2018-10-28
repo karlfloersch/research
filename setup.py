64a238d2f7ea5569465e273bd5a3e2f6884b2b8e
River <river.keefer@gmail.com>
2018-10-28 03:23:38 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="plasmalib",
+    version="0.1",
+    packages=['plasmalib'],
+
+    author="River Keefer",
+    author_email="river.keefer@gmail.com",
+)
