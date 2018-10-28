9adc4e1a304a086fa46faeaab0aa305db46d022a
River <river.keefer@gmail.com>
2018-10-28 00:20:30 +0000

    refactor test for transaction hashing

--- tests/contracts/test_tx_hash.py
+++ tests/contracts/test_tx_hash.py
@@ -11,13 +11,17 @@ def test_addr_to_bytes(w3, tester, pp):
     addr = w3.eth.defaultAccount
     assert pp.addr_to_bytes(addr) == bytes.fromhex(addr[2:])
 
+def to_bytes32(i):
+    return i.to_bytes(32, byteorder='big')
+
+
 def test_tx_hash(w3, tester, pp):
     # create an account to work with
     acct = Account.create('some entropy')
     STARTING_VALUE = 1000
     w3.eth.sendTransaction({'to': acct.address, 'value': STARTING_VALUE})
 
-    # plasma tx info
+    # plasma message info
     SENDER = w3.eth.defaultAccount
     RECIPIENT = w3.eth.defaultAccount
     START = 111323111
@@ -27,19 +31,31 @@ def test_tx_hash(w3, tester, pp):
     message = (
         addr_to_bytes(SENDER) +
         addr_to_bytes(RECIPIENT) +
-        START.to_bytes(32, byteorder='big') +
-        OFFSET.to_bytes(32, byteorder='big')
+        to_bytes32(START) +
+        to_bytes32(OFFSET)
     )
     message_hash = w3.sha3(message)
-    signature = acct.signHash(message_hash).signature
+    sig = acct.signHash(message_hash)
 
-    # confirm tx_hash is computed correctly
-    expected_tx_hash = w3.sha3(message + bytes(signature))
+    # compute expected tx hash
+    expected_tx_hash = w3.sha3(
+        addr_to_bytes(SENDER) +
+        addr_to_bytes(RECIPIENT) +
+        to_bytes32(START) +
+        to_bytes32(OFFSET) +
+        to_bytes32(sig.v) +
+        to_bytes32(sig.r) +
+        to_bytes32(sig.s)
+    )
+
+    # confirm tx hashes match
     tx_hash = pp.tx_hash(
         SENDER,
         RECIPIENT,
         START,
         OFFSET,
-        bytes(signature),
+        sig.v,
+        sig.r,
+        sig.s,
     )
     assert tx_hash == expected_tx_hash
