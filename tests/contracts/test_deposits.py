1f68433294a60f643f6a84c0d7516a2666d433de
River <river.keefer@gmail.com>
2018-10-28 10:06:24 +0000

    testing deposits

--- tests/contracts/test_deposits.py
+++ tests/contracts/test_deposits.py
@@ -4,8 +4,8 @@ from plasmalib.constants import CHALLENGE_PERIOD, PLASMA_BLOCK_INTERVAL
 
 
 def test_deposits(w3, tester, pp):
-    DEPOSIT_VALUE = 555
+    DEPOSIT_VALUE = 5551234
     for account in w3.eth.accounts:
-        pp.deposit(transact={'from': account, 'value': DEPOSIT_VALUE})
+        tx_hash = pp.deposit(transact={'from': account, 'value': DEPOSIT_VALUE})
         assert pp.deposits(account) == DEPOSIT_VALUE
     assert pp.total_deposits() == DEPOSIT_VALUE * len(w3.eth.accounts)
