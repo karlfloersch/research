038574a55dbd3286f618398a001859a922fbdb3c
Karl Floersch <karl@karlfloersch.com>
2018-12-01 14:57:10 -0500

    Use a simple serialization format for txs

--- plasmalib/operator/transactions.py
+++ plasmalib/operator/transactions.py
@@ -1,49 +1,167 @@
-import rlp
-from rlp.sedes import big_endian_int, BigEndianInt, List, CountableList
 from eth_hash.auto import keccak
-from eth.rlp.sedes import address
+from web3 import Web3
+from eth_utils import (
+    int_to_big_endian,
+    big_endian_to_int,
+)
+
+'''
+Transaction format:
+list_len + TransferRecord_1 + ... + TransferRecord_n + list_len + Signature_1 + ... + Signature_n
+
+- list_len [4 bytes]: 1 byte describing how many elements in the list, 3 bytes describing the length of each element.
+- TransferRecord [152 bytes]: 152 bytes per TransferRecord, up to `x` TransferRecords in each TX.
+- Signature [96 bytes]: 96 bytes per signature.
 
 transfer_fields = [
-    ('sender', address),
-    ('recipient', address),
-    ('start', big_endian_int),
-    ('offset', big_endian_int),
-    ('parent_start', big_endian_int),
-    ('token_id', BigEndianInt(8)),
+    ('sender', Web3.isAddress),
+    ('recipient', Web3.isAddress),
+    ('start', is_bytes32_int),
+    ('offset', is_bytes32_int),
+    ('token_id', is_bytes8_int),
+    ('max_block', is_bytes32_int),
+    ('prime', is_bytes8_int),
 ]
 sig_fields = [
-    ('v', big_endian_int),
-    ('r', big_endian_int),
-    ('s', big_endian_int),
+    ('v', is_bytes32_int),
+    ('r', is_bytes32_int),
+    ('s', is_bytes32_int),
 ]
+'''
+
+def is_bytes32_int(i):
+    b = int_to_big_endian(i)
+    if len(b) <= 32:
+        return True
+    else:
+        return False
+
+def is_bytes8_int(i):
+    b = int_to_big_endian(i)
+    if len(b) <= 8:
+        return True
+    else:
+        return False
+
+def get_field_bytes(field):
+    if field == Web3.isAddress:
+        return 20
+    elif field == is_bytes32_int:
+        return 32
+    elif field == is_bytes8_int:
+        return 8
+    else:
+        raise 'No field type checker recognized'
+
+def get_fields_total_bytes(fields):
+    fields_bytes = 0
+    for f in fields:
+        fields_bytes += get_field_bytes(f[1])
+    return fields_bytes
+
+def get_null_tx(start, offset, token_id):
+    return TransferRecord(b'\00'*20, b'\00'*20, start, offset, token_id, 0, 0)
 
 
-class TransferRecord(rlp.Serializable):
-    fields = transfer_fields
+class SimpleSerializableElement:
+    def __init__(self, *args):
+        assert self.fields is not None
+        assert len(self.fields) == len(args)
+        for i, arg in enumerate(args):
+            assert self.fields[i][1]
+            setattr(self, self.fields[i][0], arg)
+
+    def encode(self):
+        encoding = b''
+        for f in self.fields:
+            field_type = f[1]
+            field_value = getattr(self, f[0])
+            if field_type == Web3.isAddress:
+                encoding += field_value
+            elif field_type == is_bytes32_int:
+                encoding += int_to_big_endian(field_value).rjust(32, b'\0')
+            elif field_type == is_bytes8_int:
+                encoding += int_to_big_endian(field_value).rjust(8, b'\0')
+        return encoding
+
+    @classmethod
+    def decode(cls, encoding, element_type):
+        assert issubclass(element_type, cls)
+        assert len(encoding) == get_fields_total_bytes(element_type.fields)
+        args = ()
+        byte_pos = 0
+        for f in element_type.fields:
+            field_type = f[1]
+            field_bytes_len = get_field_bytes(field_type)
+            if field_type == is_bytes8_int or field_type == is_bytes32_int:
+                args += (big_endian_to_int(encoding[byte_pos:byte_pos+field_bytes_len]),)
+            else:
+                args += (encoding[byte_pos:byte_pos+field_bytes_len],)
+
+            byte_pos += field_bytes_len
+        return element_type(*args)
+
+
+class SimpleSerializableList:
+    def __init__(self, serializableElements):
+        assert len(serializableElements) > 0 and len(serializableElements) < 16
+        # Check that all elements are of the same class
+        assert all(isinstance(e, type(serializableElements[0])) for e in serializableElements)
+        self.serializableElements = serializableElements
+        self.field_total_bytes = get_fields_total_bytes(serializableElements[0].fields)
+
+    def encode(self):
+        num_elements = int_to_big_endian(len(self.serializableElements))
+        element_len = int_to_big_endian(self.field_total_bytes).rjust(3, b'\0')
+        header = num_elements + element_len
+        encoding = header
+        for ele in self.serializableElements:
+            encoding += ele.encode()
+        return encoding
 
     @property
     def hash(self) -> bytes:
-        return keccak(rlp.encode(self))
+        return keccak(self.encode())
+
+    @staticmethod
+    def decode(encoding, element_type):
+        num_elements = encoding[0]
+        element_len = big_endian_to_int(encoding[1:4])
+        elements = []
+        for i in range(num_elements):
+            start_pos = 4 + i*element_len
+            end_pos = start_pos+element_len
+            elements.append(SimpleSerializableElement.decode(encoding[start_pos:end_pos], element_type))
+        return elements
 
 
-def get_null_tx(start, offset, token_id):
-    return TransferRecord(b'\00'*20, b'\00'*20, start, offset, 0, token_id)
-
-
-class Signature(rlp.Serializable):
-    fields = sig_fields
-
-
-class Transaction(rlp.Serializable):
+class TransferRecord(SimpleSerializableElement):
     fields = [
-        ('transfers', CountableList(List([f[1] for f in transfer_fields]), 9)),
-        ('signatures', CountableList(List([f[1] for f in sig_fields]), 9)),
+        ('sender', Web3.isAddress),
+        ('recipient', Web3.isAddress),
+        ('start', is_bytes32_int),
+        ('offset', is_bytes32_int),
+        ('token_id', is_bytes8_int),
+        ('max_block', is_bytes32_int),
+        ('prime', is_bytes8_int),
     ]
 
     @property
-    def transfers_hash(self) -> bytes:
-        return keccak(rlp.encode(self.transfers))
-
-    @property
     def hash(self) -> bytes:
-        return keccak(rlp.encode(self))
+        return keccak(self.encode())
+
+
+class Signature(SimpleSerializableElement):
+    fields = [
+        ('v', is_bytes32_int),
+        ('r', is_bytes32_int),
+        ('s', is_bytes32_int),
+    ]
+
+
+class DepositRecord(SimpleSerializableElement):
+    fields = [
+        ('recipient', Web3.isAddress),
+        ('amount', is_bytes32_int),
+        ('token_id', is_bytes8_int)
+    ]
--- tests/operator/test_transactions.py
+++ tests/operator/test_transactions.py
@@ -1,25 +1,29 @@
-import rlp
 from web3 import Web3
-from plasmalib.operator.transactions import TransferRecord, get_null_tx, Signature, Transaction
+from plasmalib.operator.transactions import (
+    SimpleSerializableElement,
+    SimpleSerializableList,
+    TransferRecord,
+    Signature,
+)
 
 def test_transactions():
     sender = Web3.sha3(1)[2:22]
     recipient = Web3.sha3(2)[2:22]
-    transfer1 = TransferRecord(sender, recipient, 300, 300, 1, 250)
-    transfer2 = TransferRecord(recipient, sender, 300, 300, 1, 250)
-    sig1 = Signature(0, 0, 0)
-    sig2 = Signature(0, 0, 0)
-    tx = Transaction([transfer1.copy(), transfer2.copy()], [sig1.copy(), sig2.copy()])
-
-    print(rlp.decode(rlp.encode(transfer1), TransferRecord))
-    print(rlp.decode(rlp.encode(tx), Transaction))
-    print('~~~~')
-    print(rlp.encode(tx))
-    print('~~~~')
-    print(tx.hash)
-    print(transfer1.hash)
-    print('~~~~')
-    null_tx = get_null_tx(300, 300, 250)
-    print(rlp.decode(rlp.encode(null_tx), TransferRecord))
-    print('~~~~')
-    print(tx.transfers_hash)
+    tr1 = TransferRecord(sender, recipient, 300, 300, 1, 10, 3)
+    tr2 = TransferRecord(recipient, sender, 100, 100, 1, 10, 3)
+    sig1 = Signature(0, 1, 2)
+    sig2 = Signature(3, 4, 5)
+    tr1_encoding = tr1.encode()
+    tr1_decoded = SimpleSerializableElement.decode(tr1_encoding, TransferRecord)
+    # Verify that the decoded tx has the same hash
+    assert tr1_decoded.hash == tr1.hash
+    tr_list = SimpleSerializableList([tr1, tr2])
+    sig_list = SimpleSerializableList([sig1, sig2])
+    tr_list_encoding = tr_list.encode()
+    sig_list_encoding = sig_list.encode()
+    tr_decoded_list = SimpleSerializableList.decode(tr_list_encoding, TransferRecord)
+    sig_decoded_list = SimpleSerializableList.decode(sig_list_encoding, Signature)
+    # Verify that the hash & start pos seem to be correct
+    assert tr_decoded_list[0].hash == tr1.hash
+    assert tr_decoded_list[1].start == tr2.start
+    assert sig_decoded_list[1].v == sig2.v
