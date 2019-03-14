a91963ca0f03d06d48f057c49b2e5129ef340c28
Karl Floersch <karl@karlfloersch.com>
2019-03-14 13:32:36 -0400

    Add commit chain contract

---
+++ gen-plasma/commitment_chain_contract.py
@@ -0,0 +1,26 @@
+''' Commitment Block Structure
+{
+    subject_0: [commitment_0, commitment_1,...commitment_n],
+    subject_1: [commitment_0, commitment_1,...commitment_n],
+    ...
+    subject_n: [commitment_0, commitment_1,...commitment_n]
+}
+'''
+
+class CommitmentChain:
+    def __init__(self, operator):
+        self.operator = operator
+        self.blocks = []
+
+    def commit_block(self, msg_sender, block):
+        assert msg_sender == self.operator
+        self.blocks.append(block)
+
+    def validate_commitment(self, block_number, commitment, subject, committment_witness):
+        # Note that we are not providing merkle proofs and are instead faking it by storing the full blocks.
+        block = self.blocks[block_number]
+        # Make sure the subject contract address is in fact included in the block.
+        # NOTE: We are mocking the inclusion & so we don't actually use the commitment witness.
+        assert subject in block
+        # Return whether or not this commitment was included
+        return commitment in block[subject]
