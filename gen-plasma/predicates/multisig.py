4cfdffa85d848c267e6acd1f8b23c6b3b3c3b850
Karl Floersch <karl@karlfloersch.com>
2019-03-19 01:15:40 -0400

    Update multisig

--- gen-plasma/predicates/multisig.py
+++ gen-plasma/predicates/multisig.py
@@ -1,6 +1,7 @@
-class MultiSigTransitionWitness:
-    def __init__(self, signatures, inclusion_witness):
-        self.signatures = signatures  # Array of owner's signatures (in this it is just their names)
+class MultiSigRevocationWitness:
+    def __init__(self, next_state_commitment, signatures, inclusion_witness):
+        self.next_state_commitment = next_state_commitment
+        self.signatures = signatures
         self.inclusion_witness = inclusion_witness
 
 class MultiSigPredicate:
@@ -9,25 +10,32 @@ class MultiSigPredicate:
     def __init__(self, parent_settlement_contract):
         self.parent = parent_settlement_contract
 
-    def can_claim(self, claim, witness):
+    def can_claim(self, commitment, witness):
         # Anyone can submit a claim
         return True
 
-    def dispute_claim(self, tx_origin, old_state, transition_witness, new_state):
-        # Check these are spends of the same coin
-        assert old_state.coin_id == new_state.coin_id
+    def can_revoke(self, state_id, commitment, revocation_witness):
+        # Check the state_id is in the commitment
+        assert commitment.start <= state_id and commitment.end > state_id
+        # Check the state_id is in the revocation_witness commitment
+        assert revocation_witness.next_state_commitment.start <= state_id and revocation_witness.next_state_commitment.end > state_id
         # Check inclusion proof
-        assert self.parent.validate_inclusion(new_state, transition_witness.inclusion_witness)
-        # Check that the all valid signatures are provided
-        assert old_state.recipient == transition_witness.signatures
+        assert self.parent.commitment_chain.validate_commitment(revocation_witness.next_state_commitment,
+                                                                self.parent.address,
+                                                                revocation_witness.inclusion_witness)
+        # Check that all owners signed off on the change
+        assert commitment.state.recipient == revocation_witness.signatures
         # Check that the spend is after the claim state
-        assert old_state.plasma_block_number < new_state.plasma_block_number
+        assert commitment.plasma_block_number < revocation_witness.next_state_commitment.plasma_block_number
         return True
 
-    def resolve_claim(self, tx_origin, claim, call_data):
+    def claim_redeemed(self, claim, call_data=None):
         # Extract required information from call data
         recipients_sigs, destination = call_data
         # Check that the resolution is signed off on by all parties in the multisig
-        assert recipients_sigs == claim.state.recipient
+        assert recipients_sigs == claim.commitment.state.recipient
         # Transfer funds to the recipient
-        self.parent.erc20_contract.transferFrom(self, destination, self.parent.deposits[claim.state.coin_id].value)
+        self.parent.erc20_contract.transferFrom(self, destination, claim.commitment.end - claim.commitment.start)
+
+    def get_additional_lockup(self, state):
+        return 0
