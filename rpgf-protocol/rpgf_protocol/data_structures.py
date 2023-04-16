c46b323de971c27f8bd8bfdcc8888abb1472069c
Karl Floersch <karl@optimism.io>
2023-04-16 21:23:01 +0000

    wip modifications to data structures & prompt

--- rpgf-protocol/rpgf_protocol/data_structures.py
+++ rpgf-protocol/rpgf_protocol/data_structures.py
@@ -1,4 +1,4 @@
-from typing import List, Callable, Any, TypedDict
+from typing import List, Callable, Any, TypedDict, Set
 from datetime import time
 
 class Entity:
@@ -18,16 +18,14 @@ class User(Entity):
     citizenshipScore: int
     citizenshipVotingFunction: Callable
     rpgfVotingFunction: Callable
-
-class Collective(Entity):
-    minimumCitizenScore: int
+    impactRelationAttestationFunction: Callable
 
 class ImpactRelation:
     impactRecipient: Entity
     impactSource: Entity
     description: str
-    impact: int
-    revenue: int
+    valueCreated: int
+    valueExtracted: int
 
 class Referendum:
     start: time
@@ -64,3 +62,36 @@ class CitizenshipDistribution:
 class CitizenshipBallot:
     distribution: CitizenshipDistribution
     referendum: CitizenshipReferendum
+    citizen: User
+
+class ValueChainAttestations:
+    impactRelations: Set[ImpactRelation]
+    attestToImpactRelation: Callable[[User, ImpactRelation], None]
+
+class Collective(Entity):
+    users: List[User]
+    projects: List[Project]
+    impactCertificates: List[ImpactCertificate]
+    RPGFReferendums: List[RPGFReferendum]
+    citizenshipReferendums: List[CitizenshipReferendum]
+    attestationLayer: ValueChainAttestations
+
+
+'''
+Backup
+
+Generate a file called "run rpgf" which defines the rpgf loop.
+Notes:
+* This function should not define any business logic that we aren't really confident about, but instead rely on
+an assumption that all of the functions defined in the above data structures are defined.
+
+The file should contain the following logic and configuration:
+1. It should contain a init function which initializes the following information:
+  * A collective 
+  * A hydrateValueChain function which takes in as input a ValueChainAttestations object and a list of Users, and calls each User's impactRelationAttestationFunction passing in attestToImpactRelation
+  * A RPGFReferendum frequency and a CitizenshipReferendum frequency. This is defined in ticks (int)
+2. It should contain a "tick" function which takes in a parameter which represents time.
+3. Every tick it should call hydrateValueChain, and then every tick mod RPGFReferendum frequency call  and tick mod CitizenshipReferendum frequency
+3. If the tick mod the RPGF or citizenship frequencies, it should call a function `runRPGFReferendum` or `runCitizenshipReferendum` respectively.
+4. Running a referendum should 
+'''
